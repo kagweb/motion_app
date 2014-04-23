2b2efa33449c4226c67527fad82d56de43c828bb
Ichiro Kimura <kag.web@gmail.com>
2014-04-23 20:37:48 +0900

    side panelをとりあえず動くように

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,14 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+
+    @slider = JASidePanelController.alloc.init
+    @slider.leftPanel = LeftViewController.alloc.init
+    @slider.centerPanel = UINavigationController.alloc.initWithRootViewController(CenterViewController.alloc.init)
+
+    @window.rootViewController = @slider;
+    @window.makeKeyAndVisible
+
     true
   end
 end
